ba545b8eea67dede63d60633899baf69a98b64de
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-10-13 16:55:14 +1000

    perfect

---
+++ Task_002/Program.cs
@@ -0,0 +1,12 @@
+﻿int number1 =  20;
+int number2 = 6;
+
+if (number1%number2 == 0)
+{
+  System.Console.WriteLine("кратно");
+}
+else
+{
+  int number3 = number1%number2;
+  System.Console.WriteLine($"некратно, остаток {number3}");
+}
