918435556c58b6418ab46c95047bfd0bff55f180
Evgeniy Uslinskiy <nothgot@gmail.com>
2023-11-02 14:35:44 +1000

    ex 10

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 14, 22, 88, 48, 35, 66, 17, 88 };
+
+int n = array.Length;
+int find = 88;
+
+int index =0;
+
+while ( index<n)
+{
+    if (array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
